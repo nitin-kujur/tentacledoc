7169a44d01548592e60672c1b760f8ec1dbdcdd9
nitin kujur <kujur.nitin1992@gmail.com>
2016-07-05 11:52:09 +0530

    all working 2

--- wp-config.php
+++ wp-config.php
@@ -1,45 +1,45 @@
 <?php
 /**
- * The base configuration for WordPress
+ * The base configurations of the WordPress.
  *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
  *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
  *
  * @package WordPress
  */
 
+// ** Heroku Postgres settings - from Heroku Environment ** //
+$db = parse_url($_ENV["DATABASE_URL"]);
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sql6125723');
+define('DB_NAME', trim($db["path"],"/"));
 
 /** MySQL database username */
-define('DB_USER', 'sql6125723');
+define('DB_USER', $db["user"]);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'bnRNGlMgtE');
+define('DB_PASSWORD', $db["pass"]);
 
 /** MySQL hostname */
-define('DB_HOST', 'sql6.freemysqlhosting.net');
+define('DB_HOST', $db["host"]);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('WP_HOME','http://localhost:8080/tentacle_doc');
-define('WP_SITEURL','http://localhost:8080/tentacle_doc');
+define('WP_HOME','http://tentaclehelp.herokuapp.com/');
+define('WP_SITEURL','http://tentaclehelp.herokuapp.com/');
 define('WP_MEMORY_LIMIT', '64M');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
@@ -63,8 +63,8 @@ define('NONCE_SALT',       '^:A/7Np}^d6fJ=iu%[c]0n9JJ_^!BZ@jM,`<J*Qm*6-A<-X>.&~^
 /**
  * WordPress Database Table prefix.
  *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
 
@@ -74,11 +74,6 @@ $table_prefix  = 'wp_';
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
 
